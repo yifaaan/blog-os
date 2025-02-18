903183c51813913d78d62d6dbca16b70fb89a93e
yifaaan <0219lyf@gmail.com>
2025-02-18 14:56:18 +0800

    unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,84 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
+mod serial;
 mod vga_buffer;
 
 static HELLO: &[u8] = b"Hello, world!";
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        // in Cargo.toml, test-args = ["-device", "isa-debug-exit,iobase=0xf4,iosize=0x04"]
+        // 0xf4 is the I/O port for the QEMU ISA debug exit device
+        // 0x04 is the size of the I/O port
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+/// When not running unit tests, panic handler
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
+}
+
+/// When running unit tests, panic handler
+#[cfg(test)]
+#[panic_handler]
+fn panic_test(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T: Fn()> Testable for T {
+    fn run(&self) {
+        serial_print!("{}...\n", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+/// Entry point for unit tests
+fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("hello world{}", "!");
+    // panic!("Some panic message");
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -173,3 +173,27 @@ impl fmt::Write for Writer {
         Ok(())
     }
 }
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "test_println_output";
+    println!("{}", s);
+    println!();
+
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 3][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
