9a85c3dc7d25a3e7e5f27a9d78eff0520c7befb1
yifaaan <0219lyf@gmail.com>
2025-02-18 18:01:16 +0800

    testing

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -4,30 +4,22 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use blog_os::println;
 use core::panic::PanicInfo;
-static HELLO: &[u8] = b"Hello, world!";
 
-/// When not running unit tests, panic handler
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
-    loop {}
-}
+use blog_os::println;
 
-/// When running unit tests, panic handler
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
 }
 
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // panic!("Some panic message");
-    #[cfg(test)]
     test_main();
-
     loop {}
 }
