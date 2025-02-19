a90f3b4df8292ef85c14ad1f3b87e3bd498bbe0d
yifaaan <0219lyf@gmail.com>
2025-02-19 15:39:18 +0800

    Interpret all keys

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -67,28 +67,32 @@ extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFr
 
 /// Keyboard interrupt handler
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    use spin::Mutex;
     use x86_64::instructions::port::Port;
 
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(
+                ScancodeSet1::new(),
+                layouts::Us104Key,
+                HandleControl::Ignore
+            ));
+    }
+    let mut keyboard = KEYBOARD.lock();
     // Must read the scancode from the keyboard controller
     // to clean the interrupt status register, otherwise
     // the keyboard controller won't send another interrupt
     let mut port = Port::new(0x60);
     let scancode: u8 = unsafe { port.read() };
 
-    if let Some(key) = match scancode {
-        0x02 => Some('1'),
-        0x03 => Some('2'),
-        0x04 => Some('3'),
-        0x05 => Some('4'),
-        0x06 => Some('5'),
-        0x07 => Some('6'),
-        0x08 => Some('7'),
-        0x09 => Some('8'),
-        0x0A => Some('9'),
-        0x0B => Some('0'),
-        _ => None,
-    } {
-        print!("{}", key);
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(c) => print!("{}", c),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
     }
 
     unsafe {
