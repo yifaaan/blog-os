20f08013a8cde26010af45a5f8e1a7fea2956092
lyfQAQ <0219lyf@gmail.com>
2025-02-20 09:19:15 +0800

    Map vga buff

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use blog_os::{memory, println};
+use blog_os::{memory::{self, EmptyFrameAllocator}, println};
 
 use bootloader::{entry_point, BootInfo};
+use x86_64::structures::paging::Page;
 use core::panic::PanicInfo;
 
 /// When not running unit tests, panic handler
@@ -34,26 +35,18 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     blog_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe {
+    let mut mapper = unsafe {
         memory::init(phys_mem_offset)
     };
 
-    let address = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to the physical address 0
-        boot_info.physical_memory_offset,
-    ];
+    let mut frame_allocator = EmptyFrameAllocator;
+    
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    for &addr in address.iter() {
-        let virt = VirtAddr::new(addr);
-        let phys = mapper.translate_addr(virt);
-
-        println!("{:?}: {:?}", virt, phys);
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe {
+        page_ptr.offset(400).write_volatile(0xf021_f077_f065_f04e);
     }
 
     #[cfg(test)]
--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,6 @@
 use x86_64::{
     registers::control::Cr3,
-    structures::paging::{page_table::FrameError, PageTable, OffsetPageTable},
+    structures::paging::{page_table::FrameError, FrameAllocator, Mapper, OffsetPageTable, Page, PageTable, PhysFrame, Size4KiB},
     PhysAddr, VirtAddr,
 };
 
@@ -56,9 +56,39 @@ unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut
 //     Some(frame.start_address() + addr.page_offset().into())
 // }
 
-
+/// Initialize a new OffsetPageTable.
+///
+/// # Safety
+///
+/// This function is unsafe because it may cause a page fault.
 pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
     let level_4_table = active_level_4_table(physical_memory_offset);
     OffsetPageTable::new(level_4_table, physical_memory_offset)
 }
 
+
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        // FIXME: only for testing
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    // flush TLB
+    map_to_result.expect("map_to failed").flush();
+}
+
+
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        None
+    }
+}
